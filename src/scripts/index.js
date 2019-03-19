cc0e096ede8a3cbe90d3a4aaeeed4ad3c7c474a5
Teodor Calin Chitu <teodor.chitu@P5540.ro.softvision.global>
2019-03-19 18:39:05 +0200

    Refactoring part 2

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,83 +1,69 @@
 'use strict';
 import '../styles/index.scss';
+import * as value from './values';
+import Movie from './movie';
+const movie = new Movie();
 
-const url = 'http://localhost:3000/movies';
-const movieList = document.getElementById('movie-list');
-const movieDetails = document.getElementById('movie-container');
-const allMovies = document.getElementById('movies-container');
-const movieForm = document.getElementById('movie-form');
-movieForm.style.display = 'none';
-const movieAdd = document.getElementById('add-button');
-movieAdd.addEventListener('click', showForm);
-const submitBtn = document.getElementById('fbtn');
+value.movieForm.style.display = 'none';
 
-
-
-function showMovieList() {
-    fetch(url)
+function renderMovies() {
+    fetch(value.url)
         .then((response) => {
             return response.json();
         })
         .then((movies) => {
             movies.sort().reverse();
             movies.map((item) => {
-                //
+
                 let view = document.createElement('button');
-                view.addEventListener('click', () => {
-                    showMovie(item.id);
-                    allMovies.style.display = 'none';
-                    movieDetails.style.display = 'inherit';
-                });
                 view.id = 'view-button-' + item.id;
                 view.className = 'view-button-style';
                 view.innerText = 'VIEW';
-                //
-                let remove = document.createElement('button');
-                remove.addEventListener('click', () => {
-                    let verification = confirm('Are you sure you want to delete the following movie ?');
-                    if (verification) {
-                        deleteMovie(item.id);
-                        location.reload(true);
-                    }
+                view.addEventListener('click', () => {
+                    movie.renderMovie(item.id);
+                    value.allMovies.style.display = 'none';
+                    value.movieDetails.style.display = 'inherit';
                 });
+                let remove = document.createElement('button');
                 remove.innerText = 'DELETE';
                 remove.id = 'delete-button-' + item.id;
                 remove.className = 'delete-button-style';
-                //
+                remove.addEventListener('click', () => {
+                    let verification = confirm('Are you sure you want to delete the following movie ?');
+                    if (verification) {
+                        movie.deleteMovie(item.id);
+                        location.reload(true);
+                    }
+                });
                 let details = document.createElement('div');
                 details.className = 'movie-details';
                 details.append(view);
                 details.append(remove);
-                //
+
                 let title = document.createElement('h3');
-                title.innerText = item.title;
-                //
                 let rating = document.createElement('span');
+                title.innerText = item.title;
                 rating.className = 'rating';
                 for (let i=1; i<=5; i++) {
                     let star = document.createElement('i');
-                    // Click event
+                    let postDataFn = movie.updateData;
                     star.addEventListener("click", function(){
                         let rate = this.getAttributeNode('data-star').value;
                         item.rate += Number(rate);
                         item.nrVotes += 1;
-                        postData(item);
+                        postDataFn(item);
                     });
                     star.setAttribute('data-star', i);
                     star.className = 'fas fa-star';
                     star.id = item.id;
-
                     rating.append(star);
                 }
-                //
                 let listItem = document.createElement('li');
                 listItem.id = 'movie-item';
-                //
                 listItem.append(title);
                 listItem.append(rating);
                 listItem.append(details);
-                //
-                movieList.append(listItem);
+                value.movieList.append(listItem);
                 //
             });
         })
@@ -85,96 +71,33 @@ function showMovieList() {
             console.log(err);
         });
 }
-showMovieList();
+renderMovies();
 
-
-function showMovie(id) {
-    fetch(url + `/` + id, {
-        method: 'GET'
-    })
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            movieForm.style.display = 'none';
-            const {title, image, releaseDate, description, rate, nrVotes} = data;
-            const calc = rate/nrVotes || 0;
-            const result =
-                `   <h3>Movie title is ${title}</h3>
-                        <img alt="/" src="${image}" class="image"/>
-                        <h3>Was released on ${releaseDate}</h3>
-                        <h3>${description}</h3>
-                        <h3>${(calc).toFixed(2)}</h3>
-                        <button id="back-button">Back</button>
-                    `;
-            document.getElementById('result').innerHTML = result;
-            let backButton = document.getElementById('back-button');
-
-            backButton.addEventListener('click', function () {
-                movieDetails.style.display = 'none';
-                allMovies.style.display = 'inherit';
-            });
-        });
-}
-
-function deleteMovie(id) {
-    fetch(url + `/` + id, {
-        method: 'DELETE'
-    })
-        .then(response => response.json());
-}
-
-function showForm() {
-    movieForm.style.display = 'inline-grid';
-    movieAdd.style.display = 'none';
-    allMovies.style.display = 'none';
-    movieDetails.style.display = 'none';
-    submitBtn.addEventListener('click', () => {
+function renderForm() {
+    value.movieForm.style.display = 'inline-grid';
+    value.movieAdd.style.display = 'none';
+    value.allMovies.style.display = 'none';
+    value.movieDetails.style.display = 'none';
+    value.submitBtn.addEventListener('click', () => {
         let t = document.getElementById('title'),
             p = document.getElementById('poster'),
             r = document.getElementById('releaseDate'),
             d = document.getElementById('description');
+        const titleInput = document.getElementById('title').value,
+              posterInput = document.getElementById('poster').value,
+              releaseInput = document.getElementById('releaseDate').value,
+              descriptInput = document.getElementById('description').value;
         if(!t.checkValidity() || !p.checkValidity() || !r.checkValidity() || !d.checkValidity()){
             return false;
         } else {
-            addMovie();
+            const film = new Movie(titleInput, posterInput, releaseInput, descriptInput, 0, 0);
+            film.registerMovie();
         }
     });
 }
 
-function addMovie() {
-    const titleInput = document.getElementById('title').value;
-    const posterInput = document.getElementById('poster').value;
-    const releaseInput = document.getElementById('releaseDate').value;
-    const descriptInput = document.getElementById('description').value;
-    let movie = new FormData();
+value.movieAdd.addEventListener('click', renderForm);
 
-    movie.title = titleInput;
-    movie.image = posterInput;
-    movie.releaseDate = releaseInput;
-    movie.description = descriptInput;
-    movie.rate = 0;
-    movie.nrVotes = 0;
-    fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(movie),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    }).then(res => res.json())
-        .then(response => console.log('SUCCESS', JSON.stringify(response)))
-        .catch(error => console.error('ERROR:', error));
-}
 
-function postData(item){
-    fetch(url+ '/' + item.id, {
-        method: 'PUT',
-        body: JSON.stringify(item),
-        headers:{
-            'Content-Type': 'application/json'
-        }
-    }).then(res => res.json())
-        .then(response => console.log('SUCCESS', JSON.stringify(response)))
-        .catch(error => console.error('ERROR:', error));
-}
+
 
---
+++ src/scripts/values.js
@@ -0,0 +1,8 @@
+export const url = 'http://localhost:3000/movies',
+             movieList = document.getElementById('movie-list'),
+             movieDetails = document.getElementById('movie-container'),
+             allMovies = document.getElementById('container'),
+             movieForm = document.getElementById('form'),
+             movieAdd = document.getElementById('add-button'),
+             submitBtn = document.getElementById('fbtn');
+
